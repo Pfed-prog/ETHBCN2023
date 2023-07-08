f316a916c171d0da593c9dcf3696e24aa77c08b1
pfed-prog <fedorovn19@gmail.com>
2023-07-08 16:46:18 +0200

    builds please

--- my-app/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["www.interactivebrokers.com","www.xdc.dev"],
-  },
-};
-
-module.exports = nextConfig;
