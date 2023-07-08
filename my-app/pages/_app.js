dd2b4e31cd19cf4d7133dacae2c7d050c8133534
pfed-prog <fedorovn19@gmail.com>
2023-07-08 02:18:58 +0200

    added evm

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -1,4 +1,11 @@
 import "@/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { xdc, xdcTestnet } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
 import { Layout } from "@/components/Layout";
 import {
   Hydrate,
@@ -10,12 +17,33 @@ import { useState } from "react";
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   const [queryClient] = useState(() => new QueryClient());
 
+  const { chains, publicClient } = configureChains(
+    [xdc, xdcTestnet],
+    [publicProvider()]
+  );
+
+  const { connectors } = getDefaultWallets({
+    appName: "My RainbowKit App",
+    projectId: "YOUR_PROJECT_ID",
+    chains,
+  });
+
+  const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors,
+    publicClient,
+  });
+
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState} />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <WagmiConfig config={wagmiConfig}>
+        <RainbowKitProvider chains={chains}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </RainbowKitProvider>
+      </WagmiConfig>
     </QueryClientProvider>
   );
 }
